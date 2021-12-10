a4d0052f716760b243ae069e7990bfb8e36a63ae
Collins Adom Baffour <charisbaffour224@gmail.com>
2021-12-10 10:51:17 +0000

    Update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -25,7 +25,12 @@ export default function Header() {
         </Router>
       
          </div>
-         <button id="button2"><FontAwesomeIcon icon={faBars} /></button>
+         <button id="button2">
+         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" fill="none" viewBox="0 0 24 18">
+  <path fill="#1A1A1A" fill-rule="evenodd" d="M13.5 18a1.5 1.5 0 010-3h9a1.5 1.5 0 010 3h-9zm-12-7.5a1.5 1.5 0 010-3h21a1.5 1.5 0 010 3h-21zm6-7.5a1.5 1.5 0 010-3h15a1.5 1.5 0 010 3h-15z" clip-rule="evenodd"/>
+</svg>
+
+         </button>
 
         </nav>
     </header>
